408a4c18f0c81d086dc96d9ab590fdfc1f878513
Huugueau <dprt.hugo@gmail.com>
2025-09-17 16:19:41 +0200

    Fix in useless files x3

--- App/sources/useless/create_link.php
+++ App/sources/useless/create_link.php
@@ -4,15 +4,6 @@ $token = sha1(random_int ( 0, 99999999999999999999));
 
 $query = "UPDATE users SET chmod = '" .$token. "' WHERE mail = '" .$this->mail. "'";
 
-/*
-// retrieve token
-if (isset($_GET["token"]) && preg_match('/^[0-9A-F]{40}$/i', $_GET["token"])) {
-    $token = $_GET["token"];
-}
-else {
-    throw new Exception("Valid token not provided.");
-}
-*/
 // verify token
 $query = $db->prepare("SELECT username, tstamp FROM pending_users WHERE token = ?");
 $query->execute(array($token));
